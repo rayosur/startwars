1294ca69b9463b1de19c026f07d3a80e67692938
unknown <gonzalez.garrido@hotmail.com>
2022-04-26 00:02:04 +0200

    first commit

---
+++ src/components/Personajes.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+  
+ <img class="star" alt="Vue logo" src="../assets/Lego.png" />
+
+    <div class="characters">
+     
+    </div>
+  </section>
+</template>
+
+<script>
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  components: {},
+  setup() {
+    const store = useStore();
+    const characters = computed(() => {
+      return store.state.charactersFilter;
+    });
+    onMounted(() => {
+      store.dispatch("getCharacters");
+    });
+    return {
+      characters,
+    };
+  },
+};
+</script>
+
+<style>
+ .star{
+  width: 160px;
+}
+</style>
